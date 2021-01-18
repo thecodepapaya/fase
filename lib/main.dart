660e10cea72956e3e695afacb1671b3dc764cee4
Ashutosh Singh <thecodepapaya@gmail.com>
2021-01-19 02:15:15 +0530

    Add crude location permissions

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,23 @@
 import 'package:device_info/device_info.dart';
 import 'package:flutter/material.dart';
 import 'package:package_info/package_info.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:wifi_info_flutter/wifi_info_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
   AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
-  print('model on:   ${androidInfo.model}');
-  print('isPhysicalDevice on:  ${androidInfo.isPhysicalDevice}');
-  print('brand on:   ${androidInfo.brand}');
-  print('fingerprint on:   ${androidInfo.fingerprint}');
-  print('id on:  ${androidInfo.id}');
-  print('type on:  ${androidInfo.type}');
-  print('version on:   ${androidInfo.version.baseOS}');
-  print('androidId on:   ${androidInfo.androidId}');
-  print('device on:  ${androidInfo.device}');
-  print('tags on:  ${androidInfo.tags}');
+  // print('model on:   ${androidInfo.model}');
+  // print('isPhysicalDevice on:  ${androidInfo.isPhysicalDevice}');
+  // print('brand on:   ${androidInfo.brand}');
+  // print('fingerprint on:   ${androidInfo.fingerprint}');
+  // print('id on:  ${androidInfo.id}');
+  // print('type on:  ${androidInfo.type}');
+  // print('version on:   ${androidInfo.version.baseOS}');
+  // print('androidId on:   ${androidInfo.androidId}');
+  // print('device on:  ${androidInfo.device}');
+  // print('tags on:  ${androidInfo.tags}');
 
   var wifiBSSID = await WifiInfo().getWifiBSSID();
   var wifiIP = await WifiInfo().getWifiIP();
@@ -33,10 +34,10 @@ void main() async {
   String version = packageInfo.version;
   String buildNumber = packageInfo.buildNumber;
 
-  print(appName);
-  print(packageName);
-  print(version);
-  print(buildNumber);
+  // print(appName);
+  // print(packageName);
+  // print(version);
+  // print(buildNumber);
 
   runApp(MyApp());
 }
@@ -62,6 +63,32 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("com.iiitvadodara.fase"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          var status = await Permission.location.status;
+          if (status.isUndetermined) {
+            print("We didn't ask for permission yet");
+            PermissionStatus status = await Permission.location.request();
+            print(status);
+          }
+          // You can can also directly ask the permission about its status.
+          if (await Permission.location.isRestricted) {
+            print(
+                "The OS restricts access, for example because of parental controls");
+          }
+          if (await Permission.locationWhenInUse.serviceStatus.isEnabled) {
+            print("Location enabled");
+          }
+          if (await Permission.locationWhenInUse.serviceStatus.isDisabled) {
+            print("Location Disabled");
+          }
+        },
+        child: Icon(Icons.wifi),
+      ),
+    );
   }
 }
--- lib/startup_check.dart
+++ lib/startup_check.dart
@@ -2,28 +2,30 @@ class StartupCheck {
   /// Checks if the device is an actual device or an emulator.
   /// Emulators cannot be allowed for registering attendance.
   bool isPhysicalDevice() {
-    return false;
+    throw UnimplementedError();
   }
 
-  /// Checks if the device is rooted. Rooted devices are less trust-worthy.
+  /// Checks if the device is rooted. Rooted devices are less trust-worthy and
+  /// so it's worth collecting the info before registering attendance.
   bool isRooted() {
-    return false;
+    throw UnimplementedError();
   }
 
   /// Checks if the device is connected to a WiFi network.
   bool isWifiConnected() {
-    return false;
+    throw UnimplementedError();
   }
 
-  /// Checks if the device is connected to the institute WiFi
+  /// Checks if the device is connected to the institute WiFi. This function
+  /// checks for the obtained BSSID and WiFi name for the Institute WiFi
   bool isIIITVWifiConnected() {
-    return false;
+    throw UnimplementedError();
   }
 
   /// Checks if the application version is at-least the minimum specified by
   /// the server. This is necessary to restrict students from using older
   /// versions of the application which may contain exploitable bugs.
   bool isMinVersion() {
-    return false;
+    throw UnimplementedError();
   }
 }
